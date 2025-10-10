5fc1bd1114d001cf3f739da2d92e9256ebda1fe7
Yuxuan Li <yuxuan.li.17@ucl.ac.uk>
2025-10-10 08:55:28 +0800

    Update __init__.py

--- mmrotate/mmrotate/datasets/__init__.py
+++ mmrotate/mmrotate/datasets/__init__.py
@@ -9,10 +9,9 @@ from .rsar import RSARDataset
 from .sardet_hbb import SARDet_hbb 
 from .dior import DIORDataset
 from .diorr import DIORRDataset
-from .reobench import REOBenchDataset
 from .ssdd import SSDDDataset
 from .sardet_dota_ifred import SARDetDotaIFRedDataset
 from .sardet_hbb_trisource import SARDet_hbb_trisource
 __all__ = ['SARDataset', 'DOTADataset', 'build_dataset', 'build_dataloader', 'HRSCDataset', 'DOTAv2Dataset', 
-'FairDataset', 'RSARDataset', 'SARDet_hbb', 'DIORDataset', 'DIORRDataset', 'REOBenchDataset',
+'FairDataset', 'RSARDataset', 'SARDet_hbb', 'DIORDataset', 'DIORRDataset',
 'SSDDDataset', 'SARDetDotaIFRedDataset', 'SARDet_hbb_trisource']
